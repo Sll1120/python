17daa544e98cba1065d56d9a0d834dfc1974e37c
sll <sll917@hotmail.com>
2022-09-07 22:16:20 +0800

    add days hello

---
+++ chap0/days.py
@@ -0,0 +1,10 @@
+#======================================
+# file name:days.py
+# author:liangliangSu
+# date of writing:2022-09-06 22:19
+# description:
+#======================================
+#!/usr/bin/env python3
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+print(days)
---
+++ chap0/hello.py
@@ -0,0 +1,26 @@
+#======================================
+# file name:var.py
+# author:liangliangSu
+# date of writing:2022-09-04 17:28
+# description:
+#======================================
+#!/usr/bin/python3
+i = 5
+print (i)
+i = i + 1
+print (i)
+print('hello,world!')
+print('# 这里的内容是一个注释')
+print('''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。''')
+print("""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+""" )
+
+what_he_does = ' plays '
+his_instrument = 'guitar'
+his_name = 'Robert Johnson'
+artist_intro = his_name + what_he_does + his_instrument
+print(artist_intro)
